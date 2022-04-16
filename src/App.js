79234cde79d075358ed8b88634820d52c2b3535e
bagriffin11 <48770963+bagriffin11@users.noreply.github.com>
2022-04-16 01:28:28 -0700

    Header
    Downloaded the dependencies from ui materials. copied and pasted the header. Created new folder for drop down. Pages/links will be added to drop down.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,32 @@
 import logo from './logo.svg';
 import './App.css';
+<<<<<<< Updated upstream
+=======
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
+
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { useCollectionData } from 'react-firebase-hooks/firestore';
+
+import Main from './components/Main';
+import Login from './components/Login';
+import PrimarySearchAppBar from './components/Header';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyC5k38-_RGYp5Yo2kDebBxidGOTp2MooAQ",
+  authDomain: "lahacks-9327b.firebaseapp.com",
+  projectId: "lahacks-9327b",
+  storageBucket: "lahacks-9327b.appspot.com",
+  messagingSenderId: "236428776177",
+  appId: "1:236428776177:web:eb3b23886b37a38ab0cf50",
+  measurementId: "G-B3KLL04XKX"
+})
+>>>>>>> Stashed changes
 
 function App() {
   return (
+<<<<<<< Updated upstream
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
@@ -18,6 +42,14 @@ function App() {
           Learn React
         </a>
       </header>
+=======
+   
+    
+    <div className="App">  
+        <PrimarySearchAppBar />
+
+      {!user ? <Main /> : <Login />}
+>>>>>>> Stashed changes
     </div>
   );
 }
