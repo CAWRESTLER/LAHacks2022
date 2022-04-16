9e553df6c7e865d1a230b7141e0acea141fac456
bagriffin11 <48770963+bagriffin11@users.noreply.github.com>
2022-04-16 15:50:31 -0700

    Added Header and drop down folder.
    Right now the header is being called in the questionnaire file.

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,13 +1,17 @@
 import Button from "@mui/material/Button";
 import { logout } from "../../firebase";
+import Headerj from '../Header ';
 
 function Main(){
     return(
         <>
+    
+
         <div>   
             <Button variant="contained" onClick={logout}>Sign Out</Button>
         </div>
         <div>
+
             This is the main page
         </div>
         </>
